d9a47a2b7f75e044179a368f144edca51cae5ce7
PieroSanchez <pierosancas@gmail.com>
2024-10-15 00:05:58 -0500

    Prueba

--- src/main/java/com/sanchez/api/AdminCategoryController.java
+++ src/main/java/com/sanchez/api/AdminCategoryController.java
@@ -3,5 +3,6 @@ package com.sanchez.api;
 public class AdminCategoryController {
     void hello(){
         System.out.println("Hello from AdminCategoryController");
+        Prueba del commit que estoy haciendo
     }
 }
