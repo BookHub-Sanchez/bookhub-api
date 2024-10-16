14aa7f187bd6d7ee99c01cfc9b6263ba80bda562
PieroSanchez <pierosancas@gmail.com>
2024-10-16 12:57:33 -0500

    feat(backend): implementacion CRUD categoria

--- src/main/java/com/sanchez/service/AdminCategoryService.java
+++ src/main/java/com/sanchez/service/AdminCategoryService.java
@@ -1,5 +1,15 @@
 package com.sanchez.service;
 
+import com.sanchez.model.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import java.util.List;
+
 public interface AdminCategoryService {
-    void hello();
+    List<Category> getAll();
+    Page<Category> paginate(Pageable pageable);
+    Category findById(Integer id);
+    Category create(Category category);
+    Category update(Integer id, Category updateCategory);
+    void delete(Integer id);
 }
